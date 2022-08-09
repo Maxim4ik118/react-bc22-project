a5339489661e07ded2480dabceb875dfa872902d
Maxi <mmaximonchuk@gmail.com>
2022-08-09 21:12:11 +0300

    init

--- src/components/FeedbackOptions/Styled.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const StyledFeedbackOptions = styled.div`
-  display: flex;
-  align-items: center;
-  
-`;
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,1 @@
-export { default as FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
-export { default as Notification } from './Notification/Notification';
 export { default as Section } from './Section/Section';
-export { default as Statistics } from './Statistics/Statistics';
